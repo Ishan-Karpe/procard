34128fc91d7a10c19df79e56048b675bc4b1ed7a
Ishan-Karpe <ishan.k.abhijeet@gmail.com>
2024-11-03 11:06:54 -0800

    first commit

---
+++ card.py
@@ -0,0 +1,22 @@
+# js one of my old projects from repl.it
+
+import time
+
+print("Welcome to ProCard - A Profile Card in the Python Console!")
+time.sleep(0.5)
+
+print("Question 1 out of 5:")
+time.sleep(0.2)
+your_name = input("What is your name? ")
+print("Question 2 out of 5:")
+time.sleep(0.2)
+your_age = input("What is your age? ")
+print("Question 3 out of 5:")
+time.sleep(0.2)
+city = input("What city do you live in? ")
+print("Question 4 out of 5:")
+time.sleep(0.2)
+hobby = input("What is one hobby you like to do? ")
+print("Question 5 out of 5:")
+time.sleep(0.2)
+food = input("What is your one favorite food out of all favorite foods? ")
